1ccfc0385f081c35f2d33d20618a16adc09a4cfb
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-10-11 01:37:29 +0000

    added components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,7 +20,7 @@ return (
 <div div className = "header-links" >    
               
 <Router>
-              <Link to="/" style={{textDecoration:'none', color: 'white'}}><button>Home</button></Link>
+              <Link to="/" style={{textDecoration:'none'}}><button>Home</button></Link>
               <Link to="/login" style={{textDecoration: 'none'}}><button>Login</button></Link>
               <Link to="/signup" style={{textDecoration: 'none'}}><button>Signup</button></Link>
      
