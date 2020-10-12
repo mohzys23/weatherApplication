30158e78fcc2b99681400ae752894d79de5d1335
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-10-12 22:21:36 +0000

    added new component and css files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Header.css';
 import img from '../Assets/logo.svg';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Home from './Home';
-import Signup from './Signup';
-import Login from './Login';
+import Buttons from './Buttons';
 
 
 
@@ -19,16 +16,8 @@ return (
        <img src={img} alt="" />
     </div>
 
-<div div className = "header-links" >    
-              
-<Router>
-              <button><Link to="/" style={{textDecoration:'none', color: 'white'}}>Home</Link></button>
-              <button><Link to="/login" style={{textDecoration: 'none', color: 'white'}}>Login</Link></button>
-              <button><Link to="/signup" style={{textDecoration: 'none', color: 'white'}}>Signup</Link></button>
-
-    
-    </Router>
-
+<div div className="header-links">    
+<Buttons />
 </div>
   <div className="search-bar">
 <input type="search" name="search bar" placeholder="Search"  aria-label="search bar" />
